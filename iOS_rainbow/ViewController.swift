3f395780a9755b1fd8be23e7eee2116d97025845
Quang Nguyen <qnguyen3411@gmail.com>
2018-09-06 11:32:28 -0700

    first commit

--- iOS_rainbow/ViewController.swift
+++ iOS_rainbow/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  let colors: [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    tableView.dataSource = self
   }
-
 
 }
 
+extension ViewController: UITableViewDataSource {
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return colors.count
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+    // set text label to the model that is corresponding to the row in array
+    cell.backgroundColor = colors[indexPath.row]
+    cell.heightAnchor.constraint(equalToConstant: 120)
+    
+    // return cell so that Table View knows what to render in each row
+    return cell
+  }
+}
