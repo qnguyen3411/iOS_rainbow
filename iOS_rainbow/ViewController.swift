1e51fe2177c9dd788cd02a1a578058b0dadfb1c5
Quang Nguyen <qnguyen3411@gmail.com>
2018-09-06 11:13:36 -0700

    Initial Commit

---
+++ iOS_rainbow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS_rainbow
+//
+//  Created by Quang Nguyen on 9/6/18.
+//  Copyright © 2018 Quang Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
